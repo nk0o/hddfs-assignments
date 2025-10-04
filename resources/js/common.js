82e3f8389bb869b81d29b695e1d4882f0921801d
nk0o0o <94949582+nk0o0o@users.noreply.github.com>
2025-10-04 23:19:23 +0900

    브랜드, 상품 카드 리스트 추가

--- resources/js/common.js
+++ resources/js/common.js
@@ -1,40 +1,34 @@
 /****** Select Box ******/
-$(document).on('click', '.select__box_value', function (e) {
+$(document).on('click', '.dropdown__value', function (e) {
   const t = $(this);
   console.log(t);
-  if ($(this).parents('.select__box').hasClass('on')) {
+  if ($(this).parents('.dropdown').hasClass('on')) {
     dropDownClose(t);
   } else {
-    if ($(this).parents('.select__box').hasClass('disabled')) {
+    if ($(this).parents('.dropdown').hasClass('disabled')) {
       return false;
     }
-    $('.select__box').removeClass('on');
+    $('.dropdown').removeClass('on');
     selectBoxDown(t);
-  }
-
-  if (t.parents().hasClass('select__box_defalut')) {
-    t.change(function () {
-      t.css('color', '#222222')
-    });
   }
 });
 
-$('.select__box_list li').click(function (e) {
+$('.dropdown__list li').click(function (e) {
   selectBoxDownAction(this);
   SelectBoxChange(this);
 });
 
 function selectBoxDown(t) {
-  const $selectBox = t.parents('.select__box');
+  const $selectBox = t.parents('.dropdown');
   if (!t.hasClass('disabled')) {
     if ($selectBox.hasClass('on')) {
       $selectBox.removeClass('on')
     } else {
       $selectBox.addClass('on');
-      $selectBox.siblings('.select__box').removeClass('on');
+      $selectBox.siblings('.dropdown').removeClass('on');
     }
     $('body').on('click', function (e) {
-      if ($(e.target).closest('.select__box').length === 0 && $('.select__box').hasClass('on')) {
+      if ($(e.target).closest('.dropdown').length === 0 && $('.dropdown').hasClass('on')) {
         dropDownClose()
       }
     });
@@ -42,32 +36,32 @@ function selectBoxDown(t) {
 };
 
 function selectBoxDownAction(el) {
-  $(el).parents('.select__box_list').find('li').removeClass('selected');
+  $(el).parents('.dropdown__list').find('li').removeClass('selected');
   if (!$(el).parent('li').hasClass('disabled')) {
     $(el).addClass('selected');
   }
-  $(el).parents('.select__box').removeClass('on')
+  $(el).parents('.dropdown').removeClass('on')
 };
 
 function dropDownClose() {
-  $('.select__box').removeClass('on');
+  $('.dropdown').removeClass('on');
 };
 
 //Change Select Box Value
 function SelectBoxChange(selectItem) {
   if ($(selectItem).find('ul').length <= 0) {
-    var $cloneEle = $(selectItem).parents('.select__box').find('.select__box_value').children('span').children();
+    var $cloneEle = $(selectItem).parents('.dropdown').find('.dropdown__value').children('span').children();
     var selectText = $(selectItem).html();//250117 수정
     clearInput(selectItem);
-    $(selectItem).parents('.select__box').find('.select__box_value').children('span').html(selectText);//250117 수정
-    $(selectItem).parents('.select__box').find('.select__box_value').children('span').append($cloneEle);
-    $(selectItem).parents('.select__box').find('.select__box_value').children('span').css('color', '#222222');
+    $(selectItem).parents('.dropdown').find('.dropdown__value').children('span').html(selectText);//250117 수정
+    $(selectItem).parents('.dropdown').find('.dropdown__value').children('span').append($cloneEle);
+    $(selectItem).parents('.dropdown').find('.dropdown__value').children('span').css('color', '#222222');
   }
 };
 
 function clearInput(obj) {
-  $(obj).parents('.select__box').find('.select__box_value').children('span').children().empty();//250117 수정
-  $(obj).parents('.select__box').find('.select__box_value').children('span').empty();
+  $(obj).parents('.dropdown').find('.dropdown__value').children('span').children().empty();//250117 수정
+  $(obj).parents('.dropdown').find('.dropdown__value').children('span').empty();
 };
 
 
@@ -104,7 +98,7 @@ $(document).on('click', '.tab_menu .tab_list', function () { tabMenu(this) });
 function tabMenu(el) {
   var tab = $(el).parents('.tab_menu');
   var activeTab = $(el).attr('data-tab');
-  
+
   $(el).siblings('li').removeClass('current').attr('aria-selected', 'false');
   $(el).addClass('current').attr('aria-selected', 'true');
   $('#' + activeTab).siblings('.tab_cont_item').removeClass("current");
@@ -128,3 +122,29 @@ function tabMenu(el) {
 $(window).on('load resize', function () {
   LineTabMenuInit()
 });
+
+
+if ($('.prd-wrap.ir').length > 0) {
+  $(document).on('click', '.prd-card', function (e) {
+    e.preventDefault();
+    let imgSrc = $(this).find('img').attr('src');
+    let $parent = $(this).parent();
+    if ($parent.hasClass('checked')) {
+      $parent.removeClass('checked');
+      $parent.find('.ani_cart').remove();
+    } else {
+      $parent.addClass('checked');
+      $parent.append(
+        '<div class="ani_cart"><div class="ir_cart"><img scr="" alt=""></div></div>'
+      );
+      $parent.find('.ani_cart img').attr('src', imgSrc);
+      $parent.find('.ani_cart').addClass('active');
+      setTimeout(function () {
+        $parent.find('.ani_cart').removeClass('active');
+      }, 1000);
+    }
+    let count = $('.prd-wrap').find('.prd-card.checked').length;
+    $('.prd_cart_btn .count').text(count);
+  });
+  // putCart();
+}
