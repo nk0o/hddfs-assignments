a730d373fd5e857c24baa3378338f554616d8809
nk0o0o <94949582+nk0o0o@users.noreply.github.com>
2025-10-12 00:02:59 +0900

    장바구니 모션 추가, 마무리작업, 퍼블가이드 정리

--- resources/js/common.js
+++ resources/js/common.js
@@ -68,20 +68,20 @@ function clearInput(obj) {
 
 /**** Tab UI ****/
 
-$(document).on('click', '.tab_menu .tab_list', function () { tabMenu(this) });
+$(document).on('click', '.tab-menu .tab-menu__list', function () { tabMenu(this) });
 
 function LineTabMenuInit() {
-  var tabM = $('.tab_menu');
-  var lineTab = $('.line_tab');
-  if (tabM.hasClass("line_tab")) {
+  var tabM = $('.tab-menu');
+  var lineTab = $('.tab-menu--line');
+  if (tabM.hasClass("tab-menu--line")) {
     tabM.each(function () {
-      if ($(this).find('.tab_bar').length < 1) {
-        $(this).append("<div class='tab_bar'></div>");
+      if ($(this).find('.tab-menu__bar').length < 1) {
+        $(this).append("<div class='tab-menu__bar'></div>");
       };
     });
   }
   lineTab.each(function () {
-    $(this).find('.tab_bar').css({
+    $(this).find('.tab-menu__bar').css({
       "width": $(this).find(".current").outerWidth(),
       "left": $(this).find(".current").position().left + parseInt($(this).find(".current").css("margin-left"))
     });
@@ -89,7 +89,7 @@ function LineTabMenuInit() {
 };
 
 function tabMenu(el) {
-  var tab = $(el).parents('.tab_menu');
+  var tab = $(el).parents('.tab-menu');
   var activeTab = $(el).attr('data-tab');
 
   $(el).siblings('li').removeClass('current').attr('aria-selected', 'false');
@@ -97,23 +97,63 @@ function tabMenu(el) {
   $('#' + activeTab).siblings('.tab_cont_item').removeClass("current");
   $('#' + activeTab).addClass("current");
 
-  if (tab.hasClass("line_tab")) {
-    var tab = $(el).parents('.tab_menu');
-    var tabBar = $(el).parents('.tab_menu').find('.tab_bar');
-    if (tab.hasClass("line_tab")) {
+  if (tab.hasClass("tab-menu--line")) {
+    var tab = $(el).parents('.tab-menu');
+    if (tab.hasClass("tab-menu--line")) {
       var liWidth = tab.find(".current").outerWidth();
       var marginLeft = parseInt(tab.find(".current").css("margin-left"));
       var left = tab.find(".current").position().left + marginLeft;
       var leftss = $(tab).scrollLeft();
-      tabBar.css({
-        "width": liWidth,
-        "left": left + leftss
-      });
     }
   }
+  
+  if (tab.hasClass('tab-menu--center')) {
+    moveCenterTab(el);
+  }
 }
+//Tab 가운데
+function moveCenterTab(el){
+  let liWid = 0,
+    boxWid = $('.tab-menu--center').outerWidth(true),
+    boxHalf = $('.tab-menu--center').outerWidth(true) / 2,
+    leftPos = 0,
+    // pd = $(el).parent('ul').css('padding-left') * 2,
+    selectPos,
+    pos;
+  $(el).parents('.tab-menu--center').find('.tabmenu__list').each(function() {
+    liWid += $(this).outerWidth(true);
+  });
+  for (let i=0; i< $(el).index(); i++) {
+    leftPos += $(el).parents('.tab-menu--center').find('.tabmenu__list').eq(i).outerWidth(true);
+  }
+  selectPos = leftPos + $(el).outerWidth(true)/2;
+  if (selectPos < boxHalf) {
+  pos = 0;
+  } else if (liWid - selectPos < boxHalf) {
+  pos = liWid - boxWid;
+  }  
+  else {
+  pos = selectPos - boxHalf ;
+  }
+
+  $(el).parents('.tab-menu--center').find('>ul').animate({scrollLeft:pos});
+}
+
 $(window).on('load resize', function () {
   LineTabMenuInit()
+});
+
+//탭클릭 앵커이동
+let clickTab = $('.clickTab');
+let headerH = $('header').outerHeight();
+let tabListH = clickTab.outerHeight();
+let userAreaH = 0;
+$('.tab-menu').on('click', '.tab-menu__list', function(){
+  let idx = $(this).index();
+  let toEl = $(this).parents('.clickTab').length > 0 && $('.toCont').eq(idx);
+  $("html, body").animate({
+    scrollTop : toEl.offset().top - 24 - headerH 
+  },700)
 });
 
 /**** Modal UI ****/
@@ -122,11 +162,7 @@ $(document).on('click', '.btn--modal-open', function (e) {
   let $target = $(e.target).hasClass('btn--modal-open') ? e.target : $(e.target).closest('.btn--modal-open');
   openModal($target);
 });
-$(document).on('click', '.modal__btn-close', function (e) {
-  e.preventDefault();
-  closeModal(e.target);
-});
-$(document).on('click', '.modal__overlay', function (e) {
+$(document).on('click', '.modal__btn-close, .modal__overlay', function (e) {
   e.preventDefault();
   closeModal(e.target);
 });
@@ -166,27 +202,23 @@ function modalPosition(el) {
   }
 }
 
-/**** 장바구니 담기 ****/
-$('.prd-wrap.ir').on('click', '.prd-card', function (e) {
+/**** 상품 장바구니 담기 ****/
+$('.cart-action').on('click', '.prd-card__cart', function (e) {
   e.preventDefault();
-  let imgSrc = $(this).find('img').attr('src');
-  let $parent = $(this).parent();
-  if ($parent.hasClass('checked')) {
-    $parent.removeClass('checked');
-    $parent.find('.ani_cart').remove();
-  } else {
-    $parent.addClass('checked');
-    $parent.append(
-      '<div class="ani_cart"><div class="ir_cart"><img scr="" alt=""></div></div>'
-    );
-    $parent.find('.ani_cart img').attr('src', imgSrc);
-    $parent.find('.ani_cart').addClass('active');
-    setTimeout(function () {
-      $parent.find('.ani_cart').removeClass('active');
-    }, 1000);
-  }
-  let count = $('.prd-wrap').find('.prd-card.checked').length;
-  $('.prd_cart_btn .count').text(count);
+  let $prdCard = $(this).parents('.prd-card');
+  let $imgSrc = $prdCard.find('img').attr('src');
+  console.log($imgSrc)
+  $('.wrap').append(
+    `<div class="ani__cart">
+      <div class="ir_cart"><img scr="" alt></div>
+      <div class="cart-msg__text"><p>장바구니에 담겼습니다:)</p></div>
+    </div>`
+  );
+  $('.wrap').find('.ani__cart img').attr('src', $imgSrc);
+  $('.wrap').find('.ani__cart').addClass('active');
+  setTimeout(function () {
+    $('.wrap').find('.ani__cart').removeClass('active').remove();
+  }, 3000);
 });
 
 
@@ -210,3 +242,64 @@ function putInCartAction(){
     $('.cart-msg').removeClass('active').remove();
   },2200)
 }
+
+// 바텀시트
+ //바텀시트 열기
+  $(document).on('click', '[data-btmsheet]', function(e){
+    e.preventDefault();
+    $("#"+ $(this).data('btmsheet')).addClass('show');
+    $("body").addClass("no_scroll");
+  });
+  
+  //바텀시트 닫기
+  $(document).on('click', '.btmsheet__close, .btmsheet__back', function(e){
+    e.preventDefault();
+    closeBtmSheet(e.target)
+  });
+
+  function closeBtmSheet(el){
+    $(el).parents('.btmsheet').removeClass('show')
+    $("body").removeClass("no_scroll");
+  }
+
+  const $sheet = $('.btmsheet__inner');
+  const $handle = $sheet.find('.btmsheet__handle');
+  let startY = 0;
+  let currentY = 0;
+  let isDragging = false;
+  const threshold = 300; 
+
+  $handle.on('mousedown touchstart', function (e) {
+    startY = e.pageY || e.originalEvent.touches[0].clientY;
+    isDragging = true;
+    $sheet.css('transition', 'none');
+  });
+
+  $(document).on('mousemove touchmove', function (e) {
+    if (!isDragging) return;
+
+    const touchY = e.pageY || e.originalEvent.touches[0].clientY;
+    currentY = touchY - startY;
+
+    // 아래로 내릴 때만 작동
+    if (currentY > 0) {
+      $sheet.css('transform', `translate(-50%, ${currentY}px)`);
+    }
+  });
+
+  $(document).on('mouseup touchend', function () {
+    if (!isDragging) return;
+    isDragging = false;
+
+    $sheet.css('transition', 'transform 0.3s ease');
+
+    if (currentY > threshold) {
+      $sheet.css('transform', 'translate(-50%, 100%)');
+      closeBtmSheet($sheet)
+      $sheet.css('transform', 'translate(-50%, 0%)');
+    } else {
+      $sheet.css('transform', 'translate(-50%, 0)');
+    }
+
+    currentY = 0;
+  });
